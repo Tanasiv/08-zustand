082ea6435601625db4edeb22f3bd681c3a0f674e
Snizhana Tanasiv <snizok.t33@gmail.com>
2026-06-14 18:45:15 +0200

    hw

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,4 @@
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-import "./globals.css";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-roboto",
-  display: "swap",
-});
-
-export const metadata: Metadata = {
-  title: "NoteHub",
-  description: "Application for creating and managing notes",
-  openGraph: {
-    title: "NoteHub",
-    description: "Application for creating and managing notes",
-    url: "https://notehub.com/",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub",
-      },
-    ],
-  },
-};
+import Providers from "./providers";
 
 export default function RootLayout({
   children,
@@ -34,8 +7,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>
-        {children}
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
